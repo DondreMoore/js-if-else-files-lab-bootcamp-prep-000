9a98325de10e8ee45f6b14fb1d0435a115efd37e
Dondre Moore <raptureimmortal@icloud.com>
2019-02-23 21:46:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ if (name === "Joe") {
     console.log("The name is correct")
 }
 
-
+if (height === "74") {
+    console.log("The name is correct")
+}
     
 // Don't worry about this
 module.exports = { name, height
